26228255efd7dd877fbff03ac80f2aaa9efac5ee
Onyxmoises <99691557+Onyxmoises@users.noreply.github.com>
2023-05-10 15:27:39 -0600

    Cambio de piso por buscador

--- components/Buscador.jsx
+++ components/Buscador.jsx
@@ -1,24 +1,37 @@
 import React, { useState } from 'react'
 import styles from '../styles/Buscador.module.scss'
 import { TextField, Autocomplete, Snackbar } from "@mui/material";
+import { resolve } from 'styled-jsx/css';
 
 const Buscador = ({ onCambio, options, setOpenInfo, setRoom }) => {
 
     const [value, setValue] = useState(null);
 
-    const manejarCambio = (event) => {
+    const cInfo = (lugar, label) => new Promise((resolve, reject) => {
+        setTimeout(() => {
+            onCambio(lugar);
+            setRoom(label);
+            resolve();
+        }, 200);
+    });
+
+    const cColor = (id) => new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.querySelectorAll('.lim').forEach(element => { element.setAttribute('opacity', '0') });
+            document.querySelector(`#${id}`).setAttribute('opacity', '.7');
+            document.querySelector(`#${id}`).setAttribute('fill', '#66c0f4');
+            resolve();
+        }, 200);
+    });
+
+    const cambio = async (event) => {
+        const lugar = event.lugar;
+        const label = event.label;
         const id = event.id;
-        onCambio(event.lugar)
-        setTimeout(colorCambio, 200, id);
-        setRoom(event.label);
-    };
-    
-    const colorCambio = (id) => {
-        document.querySelectorAll('.lim').forEach(element => { element.setAttribute('opacity', '0') });
-        document.querySelector(`#${id}`).setAttribute('opacity', '.7');
-        document.querySelector(`#${id}`).setAttribute('fill', '#66c0f4');
+        await cInfo(lugar, label);
+        await cColor(id);
         setOpenInfo(true);
-    }
+    };
 
     return (
         <>
@@ -27,11 +40,11 @@ const Buscador = ({ onCambio, options, setOpenInfo, setRoom }) => {
                     <Autocomplete
                         value={value}
                         onChange={(event, newValue) => {
-                            manejarCambio(newValue);
+                            cambio(newValue);
                         }}
                         options={options}
                         groupBy={(option) => option.piso}
-                        renderInput={(params) => <TextField {...params} placeholder='Buscar'/>}
+                        renderInput={(params) => <TextField {...params} placeholder='Buscar' />}
                     />
                 </div>
             </Snackbar>

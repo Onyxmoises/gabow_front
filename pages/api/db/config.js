8178414429a403b17ecfc22c72a10a6a56e991f7
RuizEmilio <eruizb2000@alumno.ipn.mx>
2023-05-31 03:48:41 -0600

    Nueva_act

--- pages/api/db/config.js
+++ pages/api/db/config.js
@@ -1,11 +1,11 @@
 import {createPool} from "mysql2/promise";
 
 const con=createPool({
-    host:"localhost",
+    host:"containers-us-west-90.railway.app",
     user:"root",
-    password:"Hal02012()",
-    port:"3306",
-    database:"Gabow"
+    password:"5167sfiTZ0noZMSPx2p8",
+    port:"7950",
+    database:"railway"
 });
 
 export default con;

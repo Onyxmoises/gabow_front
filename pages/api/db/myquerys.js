11ad8c9e7b8a7bbfcea36753895455caef2b4cb0
Onyxmoises <99691557+Onyxmoises@users.noreply.github.com>
2023-06-02 02:34:33 -0600

    Update myquerys.js

--- pages/api/db/myquerys.js
+++ pages/api/db/myquerys.js
@@ -5,7 +5,10 @@ const myquerys ={
     insertPlace:"insert into Establecimientos(est_nombre,est_descripcion,est_horaApertura,est_horaCierre,est_paginaWeb,est_latitud,est_longitud,id_catalogo) values(?,?,?,?,?,?,?,?)",
     deletePlace:"delete from Establecimientos where id_est=?",
     selectPlaceById:"select * from Establecimientos where id_est=?",
-    updatePlace:"update Establecimientos set est_nombre=?,est_descripcion=?,est_horaApertura=?,est_horaCierre=?,est_paginaWeb=?,est_latitud=?,est_longitud=?,id_catalogo=? where id_est=?"
+    getNodeList:"select * from Nodos",
+    getSubPlace:"select * from Lugares where id_est=?",
+    insertSubplace:"insert into Lugares(nom_lug,sec_lug,id_est) values(?,?,?)",
+    insertNode:"insert into Nodos(nom_nod,lat_nod,lon_nod,id_lug,id_est) values(?,?,?,?,?)"
 }
 
 export default myquerys;

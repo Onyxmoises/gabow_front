5d928a9837c40b1a3ff15b5b77e2810f218f1bed
Edgarsc0 <esanchezc2004@alumno.ipn.mx>
2023-06-02 03:13:02 -0600

    Nueva act

--- pages/api/db/myquerys.js
+++ pages/api/db/myquerys.js
@@ -8,7 +8,8 @@ const myquerys ={
     getNodeList:"select * from Nodos",
     getSubPlace:"select * from Lugares where id_est=?",
     insertSubplace:"insert into Lugares(nom_lug,sec_lug,id_est) values(?,?,?)",
-    insertNode:"insert into Nodos(nom_nod,lat_nod,lon_nod,id_lug,id_est) values(?,?,?,?,?)"
+    insertNode:"insert into Nodos(nom_nod,lat_nod,lon_nod,id_lug,id_est) values(?,?,?,?,?)",
+    updatePlace:`updatePlace:"update Establecimientos set est_nombre=?,est_descripcion=?,est_horaApertura=?,est_horaCierre=?,est_paginaWeb=?,est_latitud=?,est_longitud=?,id_catalogo=? where id_est=?`
 }
 
 export default myquerys;

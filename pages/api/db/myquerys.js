6e5e7e6ddd6941133ff6c4000e82db8d5cdcd97a
Onyxmoises <99691557+Onyxmoises@users.noreply.github.com>
2023-06-02 02:56:38 -0600

    Front (#22)
    * Actualización de Info
    
    * Actualización Info
    
    * Obtención de información
    
    * SVGs Finales
    
    * Actualización de Img
    
    * ???
    
    * Update test.js
    
    * Chapultepec
    
    * Update Buscador.jsx
    
    * IDs SVGs
    
    * asd
    
    * Opciones

--- pages/api/db/myquerys.js
+++ pages/api/db/myquerys.js
@@ -6,9 +6,11 @@ const myquerys ={
     deletePlace:"delete from Establecimientos where id_est=?",
     selectPlaceById:"select * from Establecimientos where id_est=?",
     getNodeList:"select * from Nodos",
-    getSubPlace:"select * from Lugares where id_est=?",
-    insertSubplace:"insert into Lugares(nom_lug,sec_lug,id_est) values(?,?,?)",
-    insertNode:"insert into Nodos(nom_nod,lat_nod,lon_nod,id_lug,id_est) values(?,?,?,?,?)"
+    getSubPlace:"select label,sec_lug,sec from Lugares where id_est=?",
+    insertSubplace:"insert into Lugares(label,sec_lug,id_est) values(?,?,?)",
+    insertNode:"insert into Nodos(nom_nod,lat_nod,lon_nod,id_lug,id_est) values(?,?,?,?,?)",
+    getSpecficNode:"select nom_nod,lugs_nod from Nodos",
+    getSpecficNodeByCoordenate:"select nom_nod,lat_nod,lon_nod from Nodos"
 }
 
 export default myquerys;

ecc511e4c015e5e07670e7f3b4a0d5a84d62cb9b
Onyxmoises <99691557+Onyxmoises@users.noreply.github.com>
2023-06-05 01:39:38 -0600

    Chapultepec

--- components/InfoEdi.jsx
+++ components/InfoEdi.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React , {useState} from 'react'
 import { Button, Typography } from "@mui/material"
 import styles from '../styles/Info.module.scss';
+import axios from 'axios';
 
 const InfoEdi = ({ info }) => {
+
+  const [initial,setInital] = useState("");
+  const [destin,setDestin] = useState("");
+  const getNode = async() =>{
+
+    const node = await axios.post('/api/handlers/getSpecificnode' , {
+
+      label:info
+
+    })
+
+    if(inital != ""){
+
+      setDestin(node.data);
+      
+    }
+    else if(destin != "" && inital != ""){
+
+      setDestin(node.data)
+
+    }
+    else{
+
+      setInital(node.data);
+
+    }
+
+  }
+  
   return (
     <>
       <Typography>
@@ -11,6 +41,9 @@ const InfoEdi = ({ info }) => {
 
       <Typography>
         <Button variant="contained" href={`/`} >Regresar</Button>
+      </Typography>
+      <Typography>
+        <Button variant="contained" onClick={getNode} >Escoger Lugar</Button>
       </Typography>
     </>
   )

6214ccb6ccd93f86f6368e8fcbb0bb58524829c0
Onyxmoises <99691557+Onyxmoises@users.noreply.github.com>
2023-06-05 01:44:35 -0600

    Front (#23)
    * Actualización de Info
    
    * Actualización Info
    
    * Obtención de información
    
    * SVGs Finales
    
    * Actualización de Img
    
    * ???
    
    * Update test.js
    
    * Chapultepec
    
    * Update Buscador.jsx
    
    * IDs SVGs
    
    * asd
    
    * Opciones
    
    * Chapultepec

--- components/InfoEdi.jsx
+++ components/InfoEdi.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button, Typography } from "@mui/material"
 import styles from '../styles/Info.module.scss';
+import axios from 'axios';
 
 const InfoEdi = ({ info }) => {
   return (

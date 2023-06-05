52d823a9d157f455c5405bb815a1b35f073ce686
Onyxmoises <99691557+Onyxmoises@users.noreply.github.com>
2023-06-05 01:45:29 -0600

    ???????

---
+++ pages/api/handlers/getSpecificNode.js
@@ -0,0 +1,42 @@
+import { format } from "mysql2"
+import con from "../db/config"
+import myquerys from "../db/myquerys"
+
+const getSpecficNode = async(req , res) =>{
+    const {label} = req.body
+    try{
+        const response=await con.query(myquerys.getSpecficNode);
+        response[0].data.map(item =>{
+
+            if((item[lugs_nod].split(",")).includes(label)){
+
+                res.status(200).json({status:'OK',data:item[nom_nod]});
+
+            }
+
+        })
+    }catch(err){
+        res.status(500).json({status:'SOMETHING WENT WRONG',error:err});
+    }
+    /*con.query(myquerys.getSpecficNode, (err , response)=>{
+        if(err){
+
+            res.status(500).json({status:'SOMETHING WENT WRONG',error:err})
+        }
+        else{
+
+            response.data.map(item =>{
+
+                if((item[lugs_nod].split(",")).includes(label)){
+
+                    res.status(200).json({status:'OK',data:item[nom_nod]});
+
+                }
+
+            })
+            
+        }
+    })*/
+}
+
+export default getSpecficNode
